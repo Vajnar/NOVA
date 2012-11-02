15a8643093cfb30293306bd783daaf2603316943
Nils Asmussen <nils@os.inf.tu-dresden.de>
2012-11-02 12:36:25 +0100

    Support for the CR0.WP flag in the VTLB

--- src/vtlb.cpp
+++ src/vtlb.cpp
@@ -33,6 +33,7 @@ size_t Vtlb::gwalk (Exc_regs *regs, mword gla, mword &gpa, mword &attr, mword &e
 
     bool pse = regs->cr4_shadow & (Cpu::CR4_PSE | Cpu::CR4_PAE);
     bool pge = regs->cr4_shadow &  Cpu::CR4_PGE;
+    bool wp = regs->cr0_shadow  &  Cpu::CR0_WP;
 
     unsigned lev = max();
 
@@ -55,6 +56,9 @@ size_t Vtlb::gwalk (Exc_regs *regs, mword gla, mword &gpa, mword &attr, mword &e
             mark_pte (pte, e, TLB_A);
             continue;
         }
+
+        if (!wp && (~error & ERR_U) && (error & ERR_W))
+            attr = (attr & ~ERR_U) | ERR_W;
 
         if (EXPECT_FALSE ((attr & error) != error)) {
             error |= ERR_P;
